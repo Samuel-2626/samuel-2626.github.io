a9536191f9154b21b6965bb75e6dbd7e877b6536
Samuel Torimiro <efosonaname@gmail.com>
2025-07-14 07:20:54 -0500

    main website

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Concert+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+    <meta name="author" content="Samuel Torimrio">
+    <meta name="description" content="Software developer experienced in Python and JavaScript.">
+    <meta name="keywords" content=" Samuel Torimiro, Software Developer">
+     <meta name="robots" content="index, follow">
+    <title>Samuel Torimiro | Software Developer</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light" id="navbar-example2">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">DevSam</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#about-me">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#projects">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#articles">Articles</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#connect">Contact</a>
+        </li>
+      </ul>
+ 
+    </div>
+  </div>
+</nav>
+<main>
+<div class="p-5 pb-0 rounded-3">
+      <div class="container py-3">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="images/main.jpg" alt="Profile Image" class="main__img img-thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h1>Hello there!</h1>
+            <p>
+            My name is Samuel Torimiro, I am a passionate software developer. I aim to build applications to better serve hundreds of millions of customers.
+
+            </p>     
+
+<br>
+
+
+<a href="https://samueltorimiro.info/Samuel Torimiro Resume.pdf" class="btn btn-lg">My Resume</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <div data-target="#navbar-example2" data-spy="scroll" id="about-me">
+    <div class="container">
+    <div class="about pt-4 p-2">
+      <h2 class="text-center pb-2">About Me</h2>
+      <ul>
+        <li>Experienced in Python, JavaScript and PHP</li>
+        <li>Started web development at age 15</li>
+        <li>Written over 10+ tech articles/tutorials</li>
+        <li>Published a course on <a href="https://www.educative.io/courses/building-robust-applications-test-driven-development-django">Educative Inc.</a></li>
+      </ul>
+    </div>
+    </div>
+    </div>
+
+    <div data-target="#navbar-example2" data-spy="scroll" id="projects">
+
+    <div class="container projects">
+      <div class="mt-5">
+        <h2 class="text-center">Projects I work(ed) On</h2>
+        <br>
+        <div class="row">
+          <div class="col-md-6 col-lg-4 project pb-5">
+          <div class="card" style="width: 18rem;">
+  <img src="images/nesa_academy.jpg" class="card-img-top" alt="Nesa Academy">
+  <div class="card-body">
+    <h5 class="card-title">Nesa Academy</h5>
+    <p class="card-text mt-1">Django, Django REST Framework, Auth0, HTML, CSS, JS, and Bootstrap.</p>
+    <a href="https://github.com/Samuel-2626/project_nesa" style="float: right;"><i class="fa fa-github" style="font-size:25px"></i></a>
+    <a href="https://nesaacademy.com/" style="float: left;"><i class="fa fa-external-link" style="font-size:25px"></i></a>
+  </div>
+</div>
+          </div>
+          <div class="col-md-6 col-lg-4 project pb-5">
+          <div class="card" style="width: 18rem;">
+  <img src="images/shekelsfieldnig.jpg" class="card-img-top" alt="Shekelsfield Nigeria Limited">
+  <div class="card-body">
+    <h5 class="card-title">Shekelsfield Nig. Ltd.</h5>
+    <p class="card-text mt-1">PHP, JavaScript, HTML, CSS3, and Bootstrap.</p>
+    <a href="https://github.com/Samuel-2626/shekelsfield" style="float: right;"><i class="fa fa-github" style="font-size:25px"></i></a>
+    <a href="https://shekelsfieldnig.com/" style="float: left;"><i class="fa fa-external-link" style="font-size:25px"></i></a>
+  </div>
+</div>
+          </div>
+          <div class="col-md-6 col-lg-4 project pb-5">
+          <div class="card" style="width: 18rem;">
+  <img src="images/academic_website.jpg" class="card-img-top" alt="Academic website">
+  <div class="card-body">
+    <h5 class="card-title">Academic Website</h5>
+    <p class="card-text mt-1">PHP, JavaScript, HTML, CSS, and MaterializeCSS.</p>
+    <a href="https://github.com/Samuel-2626/academic-website" style="float: right;"><i class="fa fa-github" style="font-size:25px"></i></a>
+    <a href="https://professortorimiro.info/" style="float: left;"><i class="fa fa-external-link" style="font-size:25px"></i></a>
+  </div>
+</div>
+          </div>
+          <div class="col-md-6 col-lg-4 project pb-5">
+          <div class="card" style="width: 18rem;">
+  <img src="images/acrye.jpg" class="card-img-top" alt="ACRYE">
+  <div class="card-body">
+    <h5 class="card-title">ACRYE</h5>
+    <p class="card-text mt-1">PHP, JavaScript, HTML, CSS3, and Bootstrap.</p>
+    <a href="https://github.com/Samuel-2626/acrye" style="float: right;"><i class="fa fa-github" style="font-size:25px"></i></a>
+    <a href="https://acrye-ng.org/" style="float: left;"><i class="fa fa-external-link" style="font-size:25px"></i></a>
+  </div>
+</div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 project pb-5">
+          <div class="card" style="width: 18rem;">
+  <img src="images/pennytrust.jpg" class="card-img-top" alt="PennyTrust">
+  <div class="card-body">
+    <h5 class="card-title">PennyTrust Inc.</h5>
+
+      <p class="card-text mt-1">React, Django, Django REST Framework, HTML, CSS, and Bootstrap.</p>
+      <a href="https://github.com/Samuel-2626/PennyTrust-Frontend" style="float: right;"><i class="fa fa-github" style="font-size:25px"></i></a>
+      <a href="https://serene-volhard-8a0ea8.netlify.app/" style="float: left;"><i class="fa fa-external-link" style="font-size:25px"></i></a>
+    </div>
+</div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 project pb-5">
+          <div class="card" style="width: 18rem;">
+  <img src="images/calculator.this.jpg" class="card-img-top" alt="Calculator.this">
+  <div class="card-body">
+    <h5 class="card-title">Calculator.This</h5>
+    <p class="card-text mt-1">JavaScript, HTML, CSS, and &nbsp; &nbsp; &nbsp; &nbsp; Bootstrap.</p>
+    <a href="https://github.com/Samuel-2626/calculate.this" style="float: right;"><i class="fa fa-github" style="font-size:25px"></i></a>
+    <a href="https://thisdotcalculate.netlify.app/" style="float: left;"><i class="fa fa-external-link" style="font-size:25px"></i></a>
+    </div>
+</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <br>
+
+    <div data-target="#navbar-example2" data-spy="scroll" id="articles">
+    <div class="container">
+    <div class="articles pt-4 p-2">
+
+    <h2 class="text-center pb-2">Articles</h2>
+    <p class="text-center">Tech content that resonates with developers</p>
+    <div class="row">
+      <div class="col-md-4 pb-3"><a href="https://testdriven.io/blog/django-search/"><img src="images/1.jpg" alt="Basic and Full-text Search with Django and Postgres" class="img-fluid img-thumbnail"></a></div>
+      <div class="col-md-4 pb-3"><a href="https://testdriven.io/blog/multiple-languages-in-django/"><img src="images/2.jpg" alt="Supporting Multiple Languages in Django" class="img-fluid img-thumbnail"></a> </div>
+      <div class="col-md-4 pb-3"><a href="https://www.section.io/engineering-education/how-to-send-email-in-django/"><img src="images/3.jpg" alt="How to Send Emails in Django" class="img-fluid img-thumbnail"></a></div>
+      <div class="col-md-4 pb-3"><a href="https://www.section.io/engineering-education/full-stack-reactive-website-in-django/"><img src="images/4.jpg" alt="Full-Stack Reactive Website in Django (No JavaScript)" class="img-fluid img-thumbnail"></a></div>
+      <div class="col-md-4 pb-3"><a href="https://www.section.io/engineering-education/build-a-contact-form-with-php/"><img src="images/5.jpg" alt="Build a Contact Form with PHP" class="img-fluid img-thumbnail"></a></div>
+      <div class="col-md-4 pb-3"><a href="https://blog.replit.com/deploying-django"><img src="images/6.jpg" alt="Deploying Django to Replit" class="img-fluid img-thumbnail"></a></div>
+      <div class="col-md-4 pb-3"><a href="https://strapi.io/blog/implementing-authenticated-api-requests-to-strapi"><img src="images/7.jpg" alt="Implementing Authenticated API Requests to Strapi" class="img-fluid img-thumbnail"></a></div>
+      <div class="col-md-4 pb-3"><a href="https://community.rapyd.net/t/php-payment-gateway-integration-with-rapyd/35921"><img src="images/8.jpg" alt="PHP Payment Gateway Integration with Rapyd" class="img-fluid img-thumbnail"></a></div>
+      <div class="col-md-4 pb-3"><a href="https://testdriven.io/blog/django-pagination/"><img src="images/9.jpg" alt="Pagination in Django" class="img-fluid img-thumbnail"></a></div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <br>
+    <div class="container">
+    <hr>
+    <br>
+    <div data-target="#navbar-example2" data-spy="scroll" id="connect">
+      
+
+      
+    <div class="connect text-center">
+      <h4>Connect with me</h4>
+      <br>
+      <a href="https://github.com/Samuel-2626" class="m-3"><i class="fa fa-github" style="font-size:36px; color: #6679B6;"></i></a>
+      <a href="https://twitter.com/SamuelTorimiro4" class="m-3"><i class="fa fa-twitter" style="font-size:36px; color: #6679B6;"></i></a>
+      <a href="https://www.linkedin.com/in/samuel-torimiro" class="m-3"><i class="fa fa-linkedin" style="font-size:36px; color: #6679B6;"></i></a>
+      <a href="https://www.instagram.com/devsam1611/" class="m-3"><i class="fa fa-instagram" style="font-size:36px; color: #6679B6;"></i></a>
+    </div>
+    </div>
+    </div>
+
+    <br>
+    
+
+</main>
+
+<br>
+
+<footer>
+  <p class="text-center">Copyright &copy; 2023 Samuel Torimiro. All Rights Reserved</p>
+</footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
